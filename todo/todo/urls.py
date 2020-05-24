704eaf6398d40c000cdc3f08d53248846d3ff02a
Techinek <techinek@gmail.com>
2020-05-24 19:26:27 +0300

    Add log in. Now authentication system is completed

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     #Auth
     path('signup/', views.signup_user, name='signup_user'),
     path('logout/', views.logout_user, name='logout_user'),
+    path('login/', views.login_user, name='login_user'),
     #Todos
     path('', views.home, name='home'),
     path('current/', views.current_todos, name='current_todos'),
--- todo/todowoo/views.py
+++ todo/todowoo/views.py
@@ -1,8 +1,8 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -24,6 +24,20 @@ def signup_user(request):
                 return render(request, 'todowoo/signup_user.html', {'form': UserCreationForm(), 'error': 'That username has already been taken. Choose another one.'})
         else:
             return render(request, 'todowoo/signup_user.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'todowoo/login_user.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todowoo/login_user.html', {'form': AuthenticationForm(), 'error': 'User and password did not match'})
+        else:
+            login(request, user)
+            return redirect('current_todos')
+
+
+
 
 def logout_user(request):
     if request.method == 'POST':
