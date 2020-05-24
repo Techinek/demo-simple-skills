bdc492b3b10961d655be1b9137b6cc2ef31d6b10
Techinek <techinek@gmail.com>
2020-05-24 19:05:46 +0300

    Add log out

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
 
     #Auth
     path('signup/', views.signup_user, name='signup_user'),
+    path('logout/', views.logout_user, name='logout_user'),
     #Todos
+    path('', views.home, name='home'),
     path('current/', views.current_todos, name='current_todos'),
 ]
--- todo/todowoo/views.py
+++ todo/todowoo/views.py
@@ -2,7 +2,11 @@ from django.db import IntegrityError
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+
+
+def home(request):
+    return render(request, 'todowoo/home.html')
 
 
 def signup_user(request):
@@ -21,6 +25,10 @@ def signup_user(request):
         else:
             return render(request, 'todowoo/signup_user.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
 
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def current_todos(request):
     return render(request, 'todowoo/current_todos.html')
